1d1ada13dda3362c164afaf04f7d6b250bcbc071
Nick <ntoukas.nick@gmail.com>
2022-07-20 15:01:50 -0400

    Added StockRow component and started api config

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import './App.css';
 
+import StockRow from './components/StockRow';
+
 function App() {
-  return <div>hello world</div>;
+  return (
+    <div className='App'>
+      <div className='container'>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>Ticker</th>
+              <th>Price</th>
+              <th>Date</th>
+              <th>Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            <StockRow ticker='appl' />
+            <StockRow ticker='goog' />
+            <StockRow ticker='msft' />
+            <StockRow ticker='tsla' />
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/config/iex.js
@@ -0,0 +1,4 @@
+export const iex = {
+  api_token: 'pk_2640a81c336e4289a4bf2be0448ed692',
+  base_url: 'https://cloud.iexapis.com/stable',
+};
